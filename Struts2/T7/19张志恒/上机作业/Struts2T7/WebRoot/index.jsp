0c2266ef87296ed78397644c5fe1f30c4b85e7cb
ambrance0227 <1296660869@qq.com>
2017-12-06 15:57:37 +0800

    张志恒提交Struts2T7上机作业

---
+++ Struts2/T7/19张志恒/上机作业/Struts2T7/WebRoot/index.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <base href="<%=basePath%>">
+    
+    <title>文件的上传与下载</title>
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="expires" content="0">    
+	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+	<meta http-equiv="description" content="This is my page">
+	<!--
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	-->
+  </head>
+  
+  <body>
+    <s:form action="file!upload" method="post" enctype="multipart/form-data">
+    	<s:textfield label="用户名" name="uname" />
+    	<s:file label="选择上传的文件" name="uploadfile" />
+    	<s:submit value="确定"></s:submit>
+    </s:form>
+  </body>
+</html>
---
+++ Struts2/T7/19张志恒/上机作业/Struts2T7/src/com/qhit/ln/g4/zzh/t7/action/UploadAction.java
@@ -0,0 +1,141 @@
+/**
+ * 
+ */
+package com.qhit.ln.g4.zzh.t7.action;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author 张志恒
+ * TODO 创建上传与下载请求的action类
+ * 2017年12月5日下午2:46:35
+ */
+public class UploadAction extends ActionSupport {
+	private String uname;
+	//与上传有关的Action组件
+	private File uploadfile;
+	private String uploadfileFileName;
+	private String uploadfileContentType;
+	//与下载有关的Action组件
+	private String downfile_name;
+	private InputStream downfile_is;
+	//处理上传的请求
+	public String upload(){
+		if (uploadfile != null) {
+			try {
+				//读取上传文件的输入流
+				InputStream is = new FileInputStream(uploadfile);
+				//得到文件路径
+				String savePath = ServletActionContext.getServletContext().getRealPath("/") + "upfile";
+				File file = new File(savePath);
+				//先创建文件夹
+				if(!file.exists()){
+					file.mkdirs();
+				}
+				//保存到服务器中：将上传内容写到savePath的文件夹中
+				OutputStream os = new FileOutputStream(savePath + "/" + uploadfileFileName);
+				//写入数据
+				byte[] buffer = new byte[8096];
+				int len = 0;
+				while ((len = is.read(buffer)) != -1) {
+					os.write(buffer, 0, len);
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				super.addFieldError("index", "文件创建失败");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				super.addFieldError("index", "文件上传失败");
+			}
+		}
+		return "info";
+	}
+	//处理下载的请求
+	public String download(){
+		if(downfile_name != null){
+			try {
+				//得到文件路径
+				String savePath = ServletActionContext.getServletContext().getRealPath("/") + "upfile/" + downfile_name;
+				downfile_is = new FileInputStream(savePath);
+				return "down";
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	/**
+	 * @return the uname
+	 */
+	public String getUname() {
+		return uname;
+	}
+	/**
+	 * @param uname the uname to set
+	 */
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	/**
+	 * @return the uploadfile
+	 */
+	public File getUploadfile() {
+		return uploadfile;
+	}
+	/**
+	 * @param uploadfile the uploadfile to set
+	 */
+	public void setUploadfile(File uploadfile) {
+		this.uploadfile = uploadfile;
+	}
+	/**
+	 * @return the uploadfileFileName
+	 */
+	public String getUploadfileFileName() {
+		return uploadfileFileName;
+	}
+	/**
+	 * @param uploadfileFileName the uploadfileFileName to set
+	 */
+	public void setUploadfileFileName(String uploadfileFileName) {
+		this.uploadfileFileName = uploadfileFileName;
+	}
+	/**
+	 * @return the uploadfileContentType
+	 */
+	public String getUploadfileContentType() {
+		return uploadfileContentType;
+	}
+	/**
+	 * @param uploadfileContentType the uploadfileContentType to set
+	 */
+	public void setUploadfileContentType(String uploadfileContentType) {
+		this.uploadfileContentType = uploadfileContentType;
+	}
+	public String getDownfile_name() {
+		return downfile_name;
+	}
+	public void setDownfile_name(String downfile_name) {
+		this.downfile_name = downfile_name;
+	}
+	public InputStream getDownfile_is() {
+		return downfile_is;
+	}
+	public void setDownfile_is(InputStream downfile_is) {
+		this.downfile_is = downfile_is;
+	}
+	
+}
