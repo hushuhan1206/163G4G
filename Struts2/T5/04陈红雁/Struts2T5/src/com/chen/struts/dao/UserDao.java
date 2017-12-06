1cd6a0290f3acd3fc6fb27a4564e4ee202fbc72b
chen397404977 <chenhongyan1997@example.com>
2017-12-06 15:53:08 +0800

    1，陈红雁提交  struts2  第五章作业

---
+++ Struts2/T5/04陈红雁/Struts2T5/src/com/chen/struts/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.chen.struts.dao;
+
+import java.util.List;
+
+import com.chen.struts.bean.User;
+
+public interface UserDao {
+	public int addUser(User user);
+	public int updateUser(User user);
+	public int deleteUser(int uid);
+	public List<User> queryAllUser();
+	public User querySingleUser(int uid);
+}
