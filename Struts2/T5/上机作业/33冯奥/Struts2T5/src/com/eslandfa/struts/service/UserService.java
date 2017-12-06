405229121584fb1eb8b2501e8aa85f1634083bd6
eslandfa <1965654645@qq.com>
2017-12-06 17:21:14 +0800

    Signed-off-by: eslandfa <1965654645@qq.com> 冯奥提交作业
    Signed-off-by: eslandfa <1965654645@qq.com>
    冯奥提交作业

---
+++ Struts2/T5/上机作业/33冯奥/Struts2T5/src/com/eslandfa/struts/service/UserService.java
@@ -0,0 +1,13 @@
+package com.eslandfa.struts.service;
+
+import java.util.List;
+
+import com.eslandfa.struts.bean.User;
+
+public interface UserService {
+public int addUser(User user);
+public int updateUser(User user);
+public int deleteUser(int uid);
+public List<User> queryAllUser();
+public User querySingleUser(int uid);
+}
