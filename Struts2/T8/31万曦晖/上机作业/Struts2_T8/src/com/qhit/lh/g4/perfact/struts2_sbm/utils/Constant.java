5144a18dba4bc272aa8b29d6cc7322ee2e697f4b
PerfactM <834331256@qq.com>
2017-12-11 20:33:04 +0800

    1，万曦晖提交struts2_T8和HB_T1的上机作业

---
+++ Struts2/T8/31万曦晖/上机作业/Struts2_T8/src/com/qhit/lh/g4/perfact/struts2_sbm/utils/Constant.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.qhit.lh.g4.perfact.struts2_sbm.utils;
+
+/**
+ * @author 万曦晖
+ * 2017年11月17日
+ */
+public class Constant {
+	
+	public static final String FILE_USER_PHOTO_PATH = "files/user/pic/photo/";
+}
