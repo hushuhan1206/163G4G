c71c975a1f8eae8885227ccb9a801bf7e9524993
1024java <1661371171@qq.com>
2017-12-14 12:19:48 +0800

    1.偏晓扬，第三章作业提交

---
+++ Hibernate/T3/17偏晓扬/上机作业/Hib_T3/test/com/qhit/lh/g4/jer/test/EmpTest.java
@@ -0,0 +1,67 @@
+package com.qhit.lh.g4.jer.test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.qhit.lh.g4.jer.t3.bean.Dpt;
+import com.qhit.lh.g4.jer.t3.bean.Emp;
+import com.qhit.lh.g4.jer.t3.bean.UserInfo;
+import com.qhit.lh.g4.jer.t3.service.UserService;
+import com.qhit.lh.g4.jer.t3.service.impl.UserServiceImpl;
+
+/**
+ * @author 偏晓阳
+ * 2017年12月13日下午5:26:24
+ * TODO
+ */
+public class EmpTest {
+	
+	private UserService userService = new UserServiceImpl();
+	@Test
+	public void add() {
+		Emp emp = new Emp();
+		emp.setEmpName("二狗子");
+		emp.setEmpSex(0);
+		emp.setBirthday("2017-12-13");
+		
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUserName("egz");
+		userInfo.setPwd("123456");
+		
+		emp.setUserInfo(userInfo);
+		userInfo.setEmp(emp);
+		
+		Dpt dpt = (Dpt) userService.getObjectById(Emp.class, 2);
+		emp.setDpt(dpt);
+		
+		userService.add(emp);
+		
+	}
+	
+	public void update() {
+		Emp emp = (Emp) userService.getObjectById(Emp.class, 3);
+		emp.setEmpName("李狗蛋");
+		
+		Dpt dpt = (Dpt) userService.getObjectById(Dpt.class, 2);
+		emp.setDpt(dpt);
+		userService.update(emp);
+	}
+	
+	public void delete() {
+		
+		Emp emp = (Emp) userService.getObjectById(Emp.class, 1);
+		userService.delete(emp);
+	}
+	
+	public void query() {
+		List<Object> list = userService.queryAll("form Emp");
+		for(Object object: list){
+				Emp emp = (Emp) object;
+				System.out.println(emp.toString());
+		}
+	}
+
+}
