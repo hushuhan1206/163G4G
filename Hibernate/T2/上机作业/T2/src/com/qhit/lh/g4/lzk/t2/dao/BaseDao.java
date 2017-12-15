0270e1e67d1be65c72048bad70a4d1e02d48da5a
baidufanyi <1061257976@qq.com>
2017-12-15 11:16:24 +0800

    1，李志凯提交上机作业

---
+++ Hibernate/T2/上机作业/T2/src/com/qhit/lh/g4/lzk/t2/dao/BaseDao.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.qhit.lh.g4.lzk.t2.dao;
+
+import java.util.List;
+
+/**
+ * @author 李志凯
+ * TODO
+ * 2017年12月12日下午3:01:10
+ */
+public interface BaseDao {
+	//增
+	public void add(Object obj);
+	//删
+	public void delete(Object obj);
+	//改
+	public void update(Object obj);
+	//查
+	public List<Object> queryAll(String fromStr);
+}
