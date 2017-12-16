71e3be7deea8b4971e97aae5a877a9cdb2d8e2d0
CCfinal <1916602996@qq.com>
2017-12-16 11:02:01 +0800

    陈晨航提交作业

---
+++ Hibernate/T2/20陈晨航/上机作业/HBT2/src/com/qhit/lh/g4/djh/t2/service/impl/BaseServiceImpl.java
@@ -0,0 +1,27 @@
+package com.qhit.lh.g4.djh.t2.service.impl;
+
+import java.util.List;
+
+import com.qhit.lh.g4.djh.t2.dao.BaseDao;
+import com.qhit.lh.g4.djh.t2.dao.impl.BaseDaoImpl;
+import com.qhit.lh.g4.djh.t2.service.BaseService;
+
+public class BaseServiceImpl implements BaseService {
+	private BaseDao baseDao = new BaseDaoImpl();
+	public void add(Object obj) {
+		baseDao.add(obj);
+	}
+
+	public void delete(Object obj) {
+		baseDao.delete(obj);
+	}
+
+	public void update(Object obj) {
+		baseDao.update(obj);
+	}
+
+	public List<Object> queryAll(String tableName) {
+		return baseDao.queryAll(tableName);
+	}
+
+}
