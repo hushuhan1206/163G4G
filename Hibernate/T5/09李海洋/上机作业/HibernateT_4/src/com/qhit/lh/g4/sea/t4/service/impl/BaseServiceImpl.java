e3a4443f2411e91aecc2d2b20a0f4d31c1309b38
Seali1997 <1328694909@qq.com>
2017-12-19 15:18:39 +0800

    1，李海洋提交HibernateT5作业

---
+++ Hibernate/T5/09李海洋/上机作业/HibernateT_4/src/com/qhit/lh/g4/sea/t4/service/impl/BaseServiceImpl.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.qhit.lh.g4.sea.t4.service.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.qhit.lh.g4.sea.t4.dao.impl.BaseDaoImpl;
+import com.qhit.lh.g4.sea.t4.dao.BaseDao;
+import com.qhit.lh.g4.sea.t4.service.BaseService;
+
+/**
+ * @author 李海洋
+ *TODO
+ *2017年12月12日下午7:30:06
+ */
+public class BaseServiceImpl implements BaseService {
+	private BaseDao baseDao = new BaseDaoImpl();
+	/* (non-Javadoc)
+	 * @see com.qhit.lh.g4.sea.t4.service.BaseService#add(java.lang.Object)
+	 */
+	@Override
+	public void add(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.add(obj);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.qhit.lh.g4.sea.t4.service.BaseService#delete(java.lang.Object)
+	 */
+	@Override
+	public void delete(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.delete(obj);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.qhit.lh.g4.sea.t4.service.BaseService#update(java.lang.Object)
+	 */
+	@Override
+	public void update(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.update(obj);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.qhit.lh.g4.sea.t4.service.BaseService#getObjectById(java.lang.Class, java.io.Serializable)
+	 */
+	@Override
+	public Object getObjectById(Class clazz, Serializable id) {
+		// TODO Auto-generated method stub
+		return baseDao.getObjectById(clazz, id);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.qhit.lh.g4.sea.t4.service.BaseService#getObjects(java.lang.String)
+	 */
+	@Override
+	public List getObjects(String hql) {
+		// TODO Auto-generated method stub
+		return baseDao.getObjects(hql);
+	}
+
+}
