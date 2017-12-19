2004d06bb88ace9be1f5cdeff106cec6f03f98b6
PerfactM <834331256@qq.com>
2017-12-19 16:05:05 +0800

    1，万曦晖提交T5 上机作业

---
+++ Hibernate/T5/31万曦晖/上机作业/HB_T05/src/com/qhit/lh/g4/perfact/t5/dao/BaseDao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.qhit.lh.g4.perfact.t5.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author 万曦晖
+ *TODO
+ *2017年12月17日下午4:11:43
+ */
+public interface BaseDao {
+	public void add(Object object);
+	public void delete(Object object);
+	public void update(Object object);
+	public Object getObjectById(Class clazz,Serializable id);
+	public List<Object> queryAll(String sql);
+}
